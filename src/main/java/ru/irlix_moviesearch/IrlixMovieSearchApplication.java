18218fbb87de1b1de3cde38bc900469259718d3c
Alexandr <amakutsevich@bk.ru>
2024-08-01 22:38:28 +0700

    init commit

---
+++ src/main/java/ru/irlix_moviesearch/IrlixMovieSearchApplication.java
@@ -0,0 +1,13 @@
+package ru.irlix_moviesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrlixMovieSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrlixMovieSearchApplication.class, args);
+    }
+
+}
